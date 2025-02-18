d321d5f90fb5ad901ed445bd1457d04de4fb30f9
Francis Muniz <migueldmuniz23@gmail.com>
2025-02-18 11:52:52 -0300

    Add files via upload

---
+++ aula.4.1.2.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('#btnAdicionar')
+const tabela = document.querySelector('#tabela1')
+
+btn.addEventListener('click', ()=>{
+    let aleatorio = Math.random() * (100)+1
+    const row =
+    const data =
+})
